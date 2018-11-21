e0a1331da290a7f72ca4ea75df075d2f7c1ba654
Rene Garcia <rene.saaenz@gmail.com>
2018-11-21 01:54:18 -0600

    Adds creation of doctor

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :evolution_notes, only: [:index, :new, :create]
   end
 
+  resources :doctors
   resources :evolution_notes, only: [:show, :edit, :update, :destroy]
 end
